4bae364f2ff86992890f4b76d9277906c55a7099
robokrishan <haritkrishan176@gmail.com>
2025-10-08 02:10:15 +1100

    separate file for game constants

---
+++ BattleshipUtil.h
@@ -0,0 +1,11 @@
+#ifndef __BATTLESHIPUTIL_H__
+#define __BATTLESHIPUTIL_H__
+
+#define SIZE_GRID_ROW 10
+#define SIZE_GRID_COL 10
+
+#define MAX_NAME_LENGTH 12
+#define NUM_SHIPS 5
+#define MAX_PLAYERS 2
+
+#endif /* __BATTLESHIPUTIL_H__ */
