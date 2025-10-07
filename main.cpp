55ec0b0ad22f37a1fe965477f8c9d0b4ed3bbcd3
robokrishan <haritkrishan176@gmail.com>
2025-10-08 02:14:01 +1100

    removed redundant code

--- main.cpp
+++ main.cpp
@@ -14,27 +14,6 @@ int main() {
 
     cout << "Starting battleship..." << endl;
 
-    // // Build game grid
-    // Grid gameGrid;
-
-    // gameGrid.printGrid();
-
-    // Ship carrier = Ship(SHIP_CARRIER);
-    // Ship battleship = Ship(SHIP_BATTLESHIP);
-    // Ship cruiser = Ship(SHIP_CRUISER);
-    // Ship submarine = Ship(SHIP_SUBMARINE);
-    // Ship destroyer = Ship(SHIP_DESTROYER);
-    
-    // gameGrid.randomPlaceShip(&carrier);
-    // gameGrid.randomPlaceShip(&battleship);
-    // gameGrid.randomPlaceShip(&cruiser);
-    // gameGrid.randomPlaceShip(&submarine);
-    // gameGrid.randomPlaceShip(&destroyer);
-
-    // cout << "Randomly placed ships!" << endl << endl;
-
-    // gameGrid.printGrid();
-
     Battleship game(2);
 
     game.play();
